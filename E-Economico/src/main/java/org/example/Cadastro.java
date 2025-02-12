f75c1f2c180b8eb25b98036fdf91fb5209e508d0
paolavit <paolaqq0@icloud.com>
2025-02-12 17:52:05 -0300

    alterações na classe cadastro e usuario

--- E-Economico/src/main/java/org/example/Cadastro.java
+++ E-Economico/src/main/java/org/example/Cadastro.java
@@ -1,4 +1,30 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Cadastro {
+
+    public void cadastrar(){
+        //entrada de dados
+        Scanner scanner = new Scanner(System.in);
+
+        //dados para o cadastro
+        System.out.println("Tela de Cadastro");
+
+        System.out.print("Nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        System.out.print("Senha: ");
+        String senha = scanner.nextLine();
+
+        System.out.print("Regime de Trabalho (ex: CLT, PJ): ");
+        String regimeTrab = scanner.nextLine();
+
+        //Cria um novo usuário usando os dados fornecidos
+        Usuario novoUsuario = new Usuario(nome, email, senha, regimeTrab);
+    }
+    
 }
--- E-Economico/src/main/java/org/example/Usuario.java
+++ E-Economico/src/main/java/org/example/Usuario.java
@@ -2,7 +2,10 @@ package org.example;
 
 public class Usuario {
     private int id;
-    private String name;
+    private String nome;
+    private String email;
+    private String senha; 
+    private String regimeTrab; 
 
     public int getId() {
         return id;
@@ -12,16 +15,35 @@ public class Usuario {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getNome() {
+        return nome;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Usuario(String nome, String email, String senha, String regimeTrab){
+        this.nome = nome; 
+        this.email = email;
+        this.senha = senha;
+        this.regimeTrab = regimeTrab;
     }
 
     @Override
     public String toString() {
-        return "ID: " + id + "\tName: " + name;
+        return "ID: " + id + "\tName: " + nome;
     }
 }
